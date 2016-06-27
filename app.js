09cde312ecb67d56e7676d4e07b5311a663a208a
JackMasterson <jackmasterson5@gmail.com>
2016-06-27 00:09:20 -0400

    Get FF Info into script.js

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
+var http = require('http');
+var ejs = require('ejs');
+var express = require('express');
+var app = express();
+
+path = require('path');
+
+var fs = require('fs');
 var FFNerd = require('fantasy-football-nerd');
 var ff = new FFNerd({ api_key:  "zrdfb8pppypw"});
- 
+var playersArr = [];
+
 ff.players(function(players){
     console.log('Got players');
-    console.log(players);
+  	for(var i=0; i<players.Players.length; i++){
+
+  		var info = players.Players[i];
+  		playersArr.push({"name": info.displayName,
+						 "position": info.position,
+						 "college": info.college});
+
+  	}
 });
 
-module.exports = userRepository;
+
+//playersArr = require('/.script.js');
+
+var url = 'http://www.fantasyfootballnerd.com/service/players/json/zrdfb8pppypw/QB/';
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function(req, res, next){
+	res.json(playersArr);
+});
+
+console.log('listening on 3000!');
+app.listen(3000);
+
--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,11 @@
-//var url = 'http://www.fantasyfootballnerd.com/service/players/json/zrdfb8pppypw/QB/';
 
-var userRepository = require("userRepository.js");
 
-var userRepositoryInstance = new userRepository();  
-
-console.log(userRepository);
-//var invocation = new XMLHttpRequest();
-/*$.ajax({
-         url: "http://www.fantasyfootballnerd.com/service/players/json/zrdfb8pppypw/",
-         dataType: "json",
-         success: function(done) { console.log(done) }
-      });*/
+	$(document).ready(function() {
+		$.ajax({
+			url: "http://localhost:3000/",
+			dataType: "json",
+			success: function(data){
+				console.log(data);
+			}
+		});
+	});
